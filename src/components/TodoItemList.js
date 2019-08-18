c5cf89df3b66b5eace946b8ad2b33aca207bec74
jaejin1 <opiximeo@gmail.com>
2019-08-18 16:55:48 +0900

    component optimization

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
+
+    // 컴포넌트가 리렌더링을 할 지 말지 정해준다.
+    // 이게 없으면 항상 true 반환.
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.todos !== nextProps.todos;
+    }
+    
   render() {
     const { todos, onToggle, onRemove } = this.props;
 
